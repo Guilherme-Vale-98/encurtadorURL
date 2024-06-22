48ec3bd6e99c0e798873cdc60a1e874b008c5c3d
Guilherme Vale <guilherme.vale98@gmail.com>
2024-06-21 23:08:04 -0300

    setup fiber

--- api/main.go
+++ api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"github.com/Guilherme-Vale-98/encurtadorURL/routes"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/Guilherme-Vale-98/encurtadorURL/routes"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/lpernett/godotenv"
 )
 
 func setupRoutes(app *fiber.App){
@@ -12,5 +17,18 @@ func setupRoutes(app *fiber.App){
 }
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	app :=	fiber.New()
+
+	app.Use(logger.New())
+	setupRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+
+
 
 }
